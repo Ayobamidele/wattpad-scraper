4607d83dbcbd33700151e5218fbd37a9f029e6c1
shhossain <hossain0338@gmail.com>
2022-07-25 23:44:12 +0600

    actions change

--- main.py
+++
@@ -1,7 +0,0 @@
-from wattpad_scraper import Wattpad
-
-
-wattpad = Wattpad()
-books = wattpad.search_book("harry potter",limit=10)
-print(books[0].chapters[0].content) # on search book chapters have to load first so it may take a while
-print(len(books))
