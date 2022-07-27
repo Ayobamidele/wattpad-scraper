daffa3bb8d164408eab90e8597ff0d319b8cc98c
shhossain <hossain0338@gmail.com>
2022-07-27 18:24:59 +0000

    Version v0.0.9

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ classifiers = [
 
 setup(
     name='wattpad-scraper',
-    version='0.0.8',
+    version='0.0.9',
     description='Easy to use wattpad scraper',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
