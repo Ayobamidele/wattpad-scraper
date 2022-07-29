46f58c724887fbedf24d4e7a8352a73598f8c467
Alvaro Valenzuela <alvaro.valenzuela.alfaro@gmail.com>
2022-07-29 00:06:00 -0400

    refactor:

--- wattpad_scraper/test/test_wattpad_downloader.py
+++ wattpad_scraper/test/test_wattpad_downloader.py
@@ -1,4 +1,4 @@
-import unittest
+import pytest
 
 from wattpad_scraper import Wattpad
 
@@ -22,5 +22,5 @@ class TestWattpadDownloader:
     assert book.author.name == "haIfblood"
 
 if __name__ == "__main__":
-  unittest.main()
+  pytest.main()
   
