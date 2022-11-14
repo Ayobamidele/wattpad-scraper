ac812c69b9961b6c91e34b6e73c765d05b88580e
Sifat <hossain0338@gmail.com>
2022-11-14 21:38:32 +0600

    Tag conflict

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ classifiers = [
 
 setup(
     name='wattpad-scraper',
-    version='0.0.34',
+    version='0.0.33',
     description='Get wattpad stories and chapters, and download them as ebook',
     long_description=long_description,
     long_description_content_type='text/markdown',
