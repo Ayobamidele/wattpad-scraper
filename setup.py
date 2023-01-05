3317fb8dda17e65bff84cae8c5cbd53ce677c8ba
shhossain <hossain0338@gmail.com>
2023-01-05 17:45:10 +0000

    Version v0.0.40

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ classifiers = [
 
 setup(
     name='wattpad-scraper',
-    version='0.0.39',
+    version='0.0.40',
     description='Get wattpad stories and chapters, and download them as ebook',
     long_description=long_description,
     long_description_content_type='text/markdown',
